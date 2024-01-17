c014b8494688e6d3ed22d040b9edea200c57b035
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-01-17 14:30:51 -0600

    fixed warning issue.

---
+++ 573Goals/Views/EditMeasureGoalVC.swift
@@ -0,0 +1,29 @@
+//
+//  EditMeasureGoalVC.swift
+//  573Goals
+//
+//  Created by Kinney Kare on 1/15/24.
+//
+
+import UIKit
+
+class EditMeasureGoalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
